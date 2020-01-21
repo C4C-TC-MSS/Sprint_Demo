5e5d74e0101067412eebff9db16508c2a910a97f
Alexandre Sanscartier <alex.sanscartier@gmail.com>
2020-01-21 11:01:17 -0500

    Added real text. Modified some styling to be more consistent with the design

---
+++ TC_Sprint/Data/Certification/Certification.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TC_Sprint.Data.Certification
+{
+    public class Certification
+    {
+        public string Name { get; set; }
+        public int QualifyingServiceYears { get; set; }
+        public List<Exam> Exams { get; set; }
+        public List<Education> EducationList { get; set; }
+
+        public Certification()
+        {
+            Exams = new List<Exam>();
+            EducationList = new List<Education>();
+        }
+    }
+}
+
---
+++ TC_Sprint/Data/Certification/Exam.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TC_Sprint.Data.Certification
+{
+    public class Exam
+    {
+        public string Name { get; set; }
+        public string ExternalLink { get; set; }
+
+        public Exam()
+        {
+        }
+    }
+}
